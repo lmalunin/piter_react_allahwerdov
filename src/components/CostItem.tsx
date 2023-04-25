c75fadfec2c26f06abe23ba1c860cb50d271614b
piterpc <lmalunin@gmail.com>
2023-04-25 20:36:43 +0300

    отображение динамических данных в JSX (lesson 20)

--- src/components/CostItem.tsx
+++ src/components/CostItem.tsx
@@ -1,12 +1,16 @@
 import './CostItem.css';
 
 function CostItem() {
+    const costDate = new Date(2023, 4, 25);
+    const costDescription = 'Холодильник';
+    const costAmount = 999.99;
+
     return (
     <div className='cost-item'>
-               <div>2023</div>
+               <div>{costDate.toISOString()}</div>
                <div className='cost-item__description'>
-              <h2>Холодильник</h2>
-              <div className='cost-item__price'>$999.99</div>
+              <h2>{costDescription}</h2>
+              <div className='cost-item__price'>${costAmount}</div>
             </div>
          </div>
     );
