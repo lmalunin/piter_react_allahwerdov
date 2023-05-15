b02b98922b4a53d64f5d73073cab86d96dfdbf01
piterpc <lmalunin@gmail.com>
2023-05-15 19:29:07 +0300

    JavaScipt логика в компонентах  (lesson 22)

--- src/components/CostItem.tsx
+++ src/components/CostItem.tsx
@@ -2,10 +2,21 @@ import './CostItem.css';
 
 function CostItem(props) {
 
+
+    const month = props.date.toLocaleString('ru_RU',
+    {month: 'long'});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('ru_RU',
+    {month: '2-digits'});
+
     return (
     <div className='cost-item'>
-               <div>{props.date.toISOString()}</div>
-               <div className='cost-item__description'>
+            <div>
+               <div>{month}</div>
+               <div>{year}</div>
+               <div>{day}</div>
+            </div>
+            <div className='cost-item__description'>
               <h2>{props.description}</h2>
               <div className='cost-item__price'>${props.amount}</div>
             </div>
