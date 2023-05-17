e86f1f710e6a25fd800cb72cdb87f55a12419584
piterpc <lmalunin@gmail.com>
2023-05-17 21:47:13 +0300

    Разделение компонента на несколько компонентов  (lesson 23)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import CostItem from "./components/CostItem";
 
 function App() {
-
     const costs = [
        {
          date: new Date(2021, 2, 12),
@@ -29,19 +28,19 @@ function App() {
         date={costs[0].date}
         description={costs[0].description}
         amount={costs[0].amount}
-        ></CostItem>
+        />
 
         <CostItem
         date={costs[1].date}
         description={costs[1].description}
         amount={costs[1].amount}
-        ></CostItem>
+        />
 
         <CostItem
         date={costs[2].date}
         description={costs[2].description}
         amount={costs[2].amount}
-        ></CostItem>
+        />
     </div>
   );
 }
--- src/components/CostItem.tsx
+++ src/components/CostItem.tsx
@@ -1,21 +1,10 @@
 import './CostItem.css';
+import CostDate from "./CostDate";
 
-function CostItem(props) {
-
-
-    const month = props.date.toLocaleString('ru_RU',
-    {month: 'long'});
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString('ru_RU',
-    {month: '2-digits'});
-
+function CostItem(props:any) {
     return (
     <div className='cost-item'>
-            <div>
-               <div>{month}</div>
-               <div>{year}</div>
-               <div>{day}</div>
-            </div>
+            <CostDate date={props.date}/>
             <div className='cost-item__description'>
               <h2>{props.description}</h2>
               <div className='cost-item__price'>${props.amount}</div>
