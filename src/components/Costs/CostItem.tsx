bda13ed7d827fd6cc98bf4219c0606d54fa2b1f7
piterpc <lmalunin@gmail.com>
2023-06-09 16:06:04 +0300

    работа с SetState  (lesson 34)

--- src/components/Costs/CostItem.tsx
+++ src/components/Costs/CostItem.tsx
@@ -1,16 +1,21 @@
 import './CostItem.css';
 import CostDate from './CostDate';
 import Card from '../UI/Card';
+import React, { useState } from "react";
 
 const CostItem = (props:any) => {
+
+    const [description, setDescription] = useState(props.description);
+
     const changeDescriptionHandler = () => {
-       console.log('Click!');
+       setDescription("New cost");
+       console.log(description);
     }
     return (
     <Card className='cost-item'>
             <CostDate date={props.date}/>
             <div className='cost-item__description'>
-              <h2>{props.description}</h2>
+              <h2>{description}</h2>
               <div className='cost-item__price'>${props.amount}</div>
             </div>
             <button
