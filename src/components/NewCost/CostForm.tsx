0bfbed8a7a18beffdf2584463dbe3b49c1cd1d00
piterpc <lmalunin@gmail.com>
2023-06-22 15:24:13 +0300

    работа с несколькими состояниями  (lesson 38)

--- src/components/NewCost/CostForm.tsx
+++ src/components/NewCost/CostForm.tsx
@@ -1,8 +1,23 @@
+import React, { useState } from 'react';
 import './CostForm.css';
 
 const CostForm = () => {
+
+        const [name, setName] = useState('');
+        const [amount, setAmount] = useState('');
+        const [date, setDate] = useState('');
+
+
         const nameChangeHandler = (event:any) => {
-            console.log(event.target.value);
+            setName(event.target.value);
+        };
+
+        const amountChangeHandler = (event:any) => {
+            setAmount(event.target.value);
+        };
+
+        const dateChangeHandler = (event:any) => {
+            setDate(event.target.value);
         };
 
      return (
@@ -14,11 +29,11 @@ const CostForm = () => {
             </div>
             <div className="new-cost__control">
                 <label>Сумма</label>
-                <input type='number' min="0.01" step="0.01"/>
+                <input onChange={amountChangeHandler} type='number' min="0.01" step="0.01"/>
             </div>
             <div className="new-cost__control">
                 <label>Дата</label>
-                <input type='date' min="2023-01-01" step="2024-12-31"/>
+                <input onChange={dateChangeHandler} type='date' min="2023-01-01" step="2024-12-31"/>
             </div>
             <div className="new-cost__actions">
                  <button type="submit">Добавить Расход</button>
