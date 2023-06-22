e170e3525223ec4dc80613974bca8b0b87b7cf67
piterpc <lmalunin@gmail.com>
2023-06-22 17:11:52 +0300

    Использование одного состояния  (lesson 39)

--- src/components/NewCost/CostForm.tsx
+++ src/components/NewCost/CostForm.tsx
@@ -3,21 +3,38 @@ import './CostForm.css';
 
 const CostForm = () => {
 
-        const [name, setName] = useState('');
-        const [amount, setAmount] = useState('');
-        const [date, setDate] = useState('');
+     //   const [name, setName] = useState('');
+     //   const [amount, setAmount] = useState('');
+     //   const [date, setDate] = useState('');
 
+      const [userInput, setUserInput] =useState({
+        name: '',
+        amount: '',
+        date: ''
+      });
 
         const nameChangeHandler = (event:any) => {
-            setName(event.target.value);
+           // setName(event.target.value);
+           setUserInput({
+                ...userInput,
+                name: event.target.value
+           })
         };
 
         const amountChangeHandler = (event:any) => {
-            setAmount(event.target.value);
+           // setAmount(event.target.value);
+           setUserInput({
+                ...userInput,
+                amount: event.target.value
+           })
         };
 
         const dateChangeHandler = (event:any) => {
-            setDate(event.target.value);
+           // setDate(event.target.value);
+           setUserInput({
+                ...userInput,
+                date: event.target.value
+           })
         };
 
      return (
