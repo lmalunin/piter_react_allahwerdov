a3c6656730780bb300b86d0b73ba251d31e6c396
piterpc <lmalunin@gmail.com>
2023-06-27 15:15:21 +0300

    Обработка отправки формы  (lesson 42)

--- src/components/NewCost/CostForm.tsx
+++ src/components/NewCost/CostForm.tsx
@@ -3,9 +3,9 @@ import './CostForm.css';
 
 const CostForm = () => {
 
-        const [inputName, setName] = useState('');
-        const [inputAmount, setAmount] = useState('');
-        const [inputDate, setDate] = useState('');
+        const [inputName, setInputName] = useState('');
+        const [inputAmount, setInputAmount] = useState('');
+        const [inputDate, setInputDate] = useState('');
 
   //    const [userInput, setUserInput] = useState({
    //     name: '',
@@ -14,7 +14,7 @@ const CostForm = () => {
    //   });
 
         const nameChangeHandler = (event:any) => {
-            setName(event.target.value);
+            setInputDate(event.target.value);
           // setUserInput({
            //     ...userInput,
            //     name: event.target.value
@@ -29,7 +29,7 @@ const CostForm = () => {
         };
 
         const amountChangeHandler = (event:any) => {
-            setAmount(event.target.value);
+            setInputAmount(event.target.value);
           // setUserInput({
           //      ...userInput,
           //      amount: event.target.value
@@ -37,14 +37,14 @@ const CostForm = () => {
         };
 
         const dateChangeHandler = (event:any) => {
-            setDate(event.target.value);
+            setInputDate(event.target.value);
           // setUserInput({
           //      ...userInput,
           //      date: event.target.value
          //  })
         };
 
-        const submitHandler = (event) => {
+        const submitHandler = (event:any) => {
             event.preventDefault();
 
             const costDate = {
@@ -52,6 +52,11 @@ const CostForm = () => {
              amount: inputAmount,
              date: new Date(inputDate)
             };
+
+            console.log(costDate);
+            setInputName('');
+            setInputAmount('');
+            setInputDate('');
         };
 
      return (
@@ -59,15 +64,15 @@ const CostForm = () => {
         <div className="new-cost__controls">
             <div className="new-cost__control">
                 <label>Название</label>
-                <input type='text' onChange={nameChangeHandler}/>
+                <input type='text' value={inputName} onChange={nameChangeHandler}/>
             </div>
             <div className="new-cost__control">
                 <label>Сумма</label>
-                <input onChange={amountChangeHandler} type='number' min="0.01" step="0.01"/>
+                <input value={inputAmount} onChange={amountChangeHandler} type='number' min="0.01" step="0.01"/>
             </div>
             <div className="new-cost__control">
                 <label>Дата</label>
-                <input onChange={dateChangeHandler} type='date' min="2023-01-01" step="2024-12-31"/>
+                <input value={inputDate} onChange={dateChangeHandler} type='date' min="2023-01-01" step="2024-12-31"/>
             </div>
             <div className="new-cost__actions">
                  <button type="submit">Добавить Расход</button>
